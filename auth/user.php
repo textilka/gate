cfd4d15154f5a6c8cf1ab47d55e631d5591472c4
keombre <keombre8@gmail.com>
2018-09-03 00:05:39 +0200

    destroy session

--- auth/user.php
+++ auth/user.php
@@ -41,6 +41,9 @@ class user {
     }
 
     function logout() {
+        if (isset($_COOKIE[session_name()]))
+            setcookie( session_name(), "", time()-3600, "/");
+        $_SESSION = array();
         session_destroy();
         header("Location: " . creds::LOGOUT);
         die();
