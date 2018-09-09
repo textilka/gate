34276478469dd741d090a508b87b243666cd4dbf
keombre <keombre8@gmail.com>
2018-09-09 03:37:38 +0200

    added backed for memory usage API

--- gate/index.php
+++ gate/index.php
@@ -3,6 +3,8 @@
 require __DIR__ . "/../vendor/autoload.php";
 require __DIR__ . "/../auth/user.php";
 
+$admins = ["Domain Admins", "Enterpise Admins"];
+
 $user = new auth\user();
 
 if ($user->isLogged()) {
@@ -12,17 +14,43 @@ if ($user->isLogged()) {
     if (isset($_GET['vpn'])) {
         $back = true;
         include __DIR__ . "/../template/vpn.phtml";
+
     } else if (isset($_GET['logout'])) {
         $user->logout();
+
+    } else if (isset($_GET['api'])) {
+        if ($_GET['api'] == "load") {
+            foreach ($user->getUserData()['groups'] as $group) {
+                if (in_array($group, $admins)) {
+                    $data = [
+                        "data" => [
+                            "cpu" => sys_getloadavg(),
+                            "mem" => getSystemMemInfo(),
+                        ],
+                        "status" => "success"
+                    ];
+                    exit(json_encode(array_merge($data)));
+                }
+            }
+            exit(json_encode(["status" => "error"]));
+        }
+        exit(json_encode(["status" => "error"]));
+        
     } else {
         include __DIR__ . "/../template/app.phtml";
     }
-} else {
 
-    // hard redirect, we don't need login button
-    //if (isset($_GET['login'])) {
-        header("Location: login.php");
-    //} else {
-    //    include __DIR__ . "/../template/login.phtml";
-    //}
+} else {
+    header("Location: login.php");
+
+}
+
+function getSystemMemInfo() {
+    $data = explode("\n", file_get_contents("/proc/meminfo"));
+    $meminfo = [];
+    foreach ($data as $line) {
+        list($key, $val) = explode(":", $line);
+        $meminfo[$key] = trim($val);
+    }
+    return $meminfo;
 }
